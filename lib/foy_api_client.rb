d3f09509a5d6823f4bc669b69cabf276428a64e1
Roberto Soares <roberto.tech@gmail.com>
2014-01-27 17:30:28 -0200

    added get_projects (draft version)

--- lib/foy_api_client.rb
+++ lib/foy_api_client.rb
@@ -1,8 +1,19 @@
 require 'foy_api_client/version'
+require 'rest-client'
 
 module Foy
   module API
     module Client
+      class Base
+        def self.get_projects
+          json = RestClient.get 'http://localhost:5000/v1/projects.json'
+          data = JSON.parse(json)
+
+          data.collect do |element|
+            OpenStruct.new(element)
+          end
+        end
+      end
     end
   end
 end
---
+++ spec/get_projects_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Foy::API::Client::Base do
+  describe ".get_projects" do
+    let(:data) do
+      {
+        id: "2",
+        repository: "New Repo",
+        title: "New Project"
+      }
+    end
+
+    let(:data_json) do
+      [data].to_json
+    end
+
+    before do
+      stub_request(:get, "http://localhost:5000/v1/projects.json").
+         to_return(status: 200, body: data_json)
+    end
+
+    it "calls /v1/projects.json" do
+      Foy::API::Client::Base.get_projects
+    end
+
+    it "returns an array" do
+      result = Foy::API::Client::Base.get_projects
+      expect(result).to be_an(Array)
+    end
+
+    it "returns data as OpenStruct" do
+      result = Foy::API::Client::Base.get_projects
+      expect(result.first).to be == OpenStruct.new(data)
+    end
+  end
+end
